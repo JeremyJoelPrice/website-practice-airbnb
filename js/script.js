d57b6e553e1ca5f07bff3ba49c9a28c38567ca79
Jeremy Price <jeremyjoelprice@gmail.com>
2021-03-11 16:32:33 +0000

    footer added

--- js/script.js
+++ js/script.js
@@ -2,4 +2,8 @@
  addContent("#header-content", "snippets/header-content.html")
  addContent("#main-content", "snippets/main-content.html")
  addContent("#footer-content", "snippets/footer-content.html")
+ addContent("#vector-global", "snippets/vector graphics/global.html")
+ addContent("#vector-facebook", "snippets/vector graphics/facebook.html")
+ addContent("#vector-twitter", "snippets/vector graphics/twitter.html")
+ addContent("#vector-instagram", "snippets/vector graphics/instagram.html")
 })();
